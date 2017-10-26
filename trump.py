18187bda11e15c7a46f2b32a59decbe61b866d0c
CarlsKe <fernancarldiaz@outlook.com>
2017-10-26 11:37:52 -0500

    feat: created dataframe for csv files

--- trump.py
+++ trump.py
@@ -18,15 +18,36 @@ expected_files = [
 tweets = pd.read_csv(expected_files[0])
 news = pd.read_csv(expected_files[1])
 
-#issue
-
+#############
+# issue
+#############
 
 issue1 = pd.read_csv(expected_files[2])
 issue2 = pd.read_csv(expected_files[3])
 issue3 = pd.read_csv(expected_files[4])
 issue4 = pd.read_csv(expected_files[5])
 issue5 = pd.read_csv(expected_files[6])
+
 pres = pd.read_csv(expected_files[7])
 us = pd.read_csv(expected_files[8])
 
-print(us)
+##############
+# DataFrame
+##############
+
+dtTweets = pd.DataFrame(tweets)
+#print("\n"*4)
+dtnews = pd.DataFrame(news)
+#print("\n"*4)
+
+##################
+# DataFrame issue
+##################
+
+dtIssue1 = pd.DataFrame(issue1)
+dtIssue2 = pd.DataFrame(issue2)
+dtIssue3 = pd.DataFrame(issue3)
+dtIssue4 = pd.DataFrame(issue4)
+dtIssue5 = pd.DataFrame(issue5)
+
+print(dtIssue1[['net_approval']].values.sum())
