4f259aee0004354df47092aa1f1ce7ed686a359e
CarlsKe <fernancarldiaz@outlook.com>
2017-10-30 17:59:00 -0600

    chore: changed desaing scrap Class

--- scraping/scrap.py
+++ scraping/scrap.py
@@ -7,27 +7,30 @@ from tabulate import tabulate
 from pymongo import MongoClient
 
 
-class MongoDB():
-    def __int__(self):
-        pass
+class Scrap:
 
-    def showConections(self):
-        pass
+    def __init__(self):
+        """Initializes the data."""
+        self.client = MongoClient('localhost', 27017)
+        print("(Initializing {})".format(self.client))
+
+    def showDB(self):
+        print(self.client.database_names())
+
+    def saveConllections(self):
+        print('Save collections')
+
+    def showCollections(self):
+        print('Show Collections')
+
+    def get_info(self, contrib):
+        res = requests.get("https://es.wikipedia.org/wiki/"+contrib)
+        soup = BeautifulSoup(res.content, 'lxml')
+        table = soup.find_all('table')[0]
+        df = pd.read_html(str(table))
+        print(tabulate(df[0], headers='keys', tablefmt='psql'))
 
 
-def get_info(Contrib):
-    res = requests.get("https://es.wikipedia.org/wiki/"+Contrib)
-    soup = BeautifulSoup(res.content, 'lxml')
-    table = soup.find_all('table')[0]
-    df = pd.read_html(str(table))
-    print(tabulate(df[0], headers='keys', tablefmt='psql'))
-
-
-# Demo connect
-client = MongoClient('localhost',27017)
-db = client.database_names()
-print(db)
-
-print(client)
-
-
+sp = Scrap()
+sp.showDB()
+sp.get_info("Alemania")
